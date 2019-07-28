cd46a0daad6d5e21ed31e16413ba909f21acf657
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2019-07-28 15:37:00 +0200

    added setItem and getItems script

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,28 @@
 
 
-const secret = {
-    name: '',
-    age: '',
-    text: '',
-}
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const txtArea = document.getElementById('secret');
+const saveButton = document.querySelector('.form__input--Js2');
+const loadButton = document.querySelector('.form__input--Js1');
 
-const nameInput = document.querySelector('.form__input--nameJs');
-const ageInput = document.querySelector('.form__input--ageJs');
-const txtArea = document.querySelector('.form__input--txtAreaJs')
-const saveButton = document.querySelector('.form__input--Js1');
-const loadButton = document.querySelector('.form__input--Js2');
 
 saveButton.addEventListener('click', function () {
-    localStorage.setItem('nameInput', JSON.stringify(nameInput));
-    localStorage.setItem('ageInput', JSON.stringify(ageInput));
-    localStorage.setItem('txtArea', JSON.stringify(txtArea));
-})
+     localStorage.setItem('name', nameInput.value);
+     localStorage.setItem('age', ageInput.value);
+     localStorage.setItem('secret', txtArea.value);
+ })
+
+
+/* saveButton.addEventListener('click', function () {
+     localStorage.setItem('name', JSON.stringify(nameInput.value));
+     localStorage.setItem('age', JSON.stringify(ageInput.value));
+     localStorage.setItem('secret', JSON.stringify(txtArea.value));
+}) */
+
+
+loadButton.addEventListener('click', function () {
+    nameInput.value = localStorage.getItem('name');
+    ageInput.value = localStorage.getItem('age');
+    txtArea.value = localStorage.getItem('secret');
+});
