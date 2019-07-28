ffa208c493d96bc33b32332cb17624b55a0f9423
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2019-07-28 12:42:32 +0200

    first js commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
-"use strict";
 
-console.log(`Hello world!`);
+
+const secret = {
+    name: '',
+    age: '',
+    text: '',
+}
+
+const nameInput = document.querySelector('.form__input--nameJs');
+const ageInput = document.querySelector('.form__input--ageJs');
+const txtArea = document.querySelector('.form__input--txtAreaJs')
+const saveButton = document.querySelector('.form__input--Js1');
+const loadButton = document.querySelector('.form__input--Js2');
+
+saveButton.addEventListener('click', function () {
+    localStorage.setItem('nameInput', JSON.stringify(nameInput));
+    localStorage.setItem('ageInput', JSON.stringify(ageInput));
+    localStorage.setItem('txtArea', JSON.stringify(txtArea));
+})
